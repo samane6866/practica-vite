c1f29f09714be6aae74ebf796d9af2a54508e746
samane6866 <samane.bahramian@gmail.com>
2023-05-18 13:21:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Tarjeta from "./Tarjeta";
+import PackingList from "./PackingList";
+
+function App() {
+  return (
+    <div>
+      <h1>React</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi,
+        voluptatem?
+      </p>
+      <Tarjeta />
+      <PackingList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tarjeta.jsx
@@ -0,0 +1,11 @@
+function Tarjeta() {
+  return (
+    <div>
+      <h2>soy una tarjeta</h2>
+      <p>Tajeta soy,descrippcion doy</p>
+      <Tarjeta />
+      <Tarjeta />
+    </div>
+  );
+}
+export default Tarjeta;
